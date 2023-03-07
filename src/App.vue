4b1460909a2fc6110eaba5f3da85c9c128eb7edc
Lapo0 <lapo.gini.01@gmail.com>
2023-03-07 16:42:24 +0100

    START

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+    import { def } from '@vue/shared';
+
+    import AppHeader from '/src/components/AppHeader.vue'
+
+
+    export default {
+        components: {
+          AppHeader,
+        }
+    }
+
+</script>
+
+<template>
+
+  <AppHeader />
+
+
+</template>
+
+<style lang="scss">
+
+    @use '/src/style/general.scss';
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <header>
+        <div class="container">
+            <div class="row">
+                <h1>
+                    BOOLFLIX
+                </h1>
+                <div class="input-search">
+                    <input type="text" placeholder="Cerca il film che fa per te...">
+                </div>
+            </div>
+        </div>
+    </header>
+
+</template>
+
+
+<script>
+
+    export default {
+
+    }
+
+</script>
+
+
+
+<style lang="scss">
+
+    .row {
+        align-items: center;
+        justify-content: space-between;
+
+        input {
+            line-height: 40px;
+            font-size: 20px;
+            border-radius: 25px;
+            padding: 0 20px;
+        }
+    }
+
+    
+
+</style>
